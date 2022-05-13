0b77b66bf2bc8cd60dac3cc849e6eec80d1ebc68
Hemant <jainhemant126@gmail.com>
2022-05-14 00:51:13 +0530

    Marks update route implemented

--- backend/routes/marks.js
+++ backend/routes/marks.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const student = require('../controller/markscontroller')
 
 router.route('/').get(student.getstudentmarks)
-router.route('/').post(student.poststudentmarks)
 router.route('/:roll').get(student.specificstudent)
 router.route('/:roll/sessions').get(student.totalsessions)
 router.route('/:roll/:session').get(student.specificsessionmarks)
+router.route('/:roll/:session/new').post(student.updatestudentmarks)
 router.route('/:roll/:session/subjects').get(student.specificsubjects)
 
 module.exports = router;
