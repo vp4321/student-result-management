fdaf7cd26a8c9c67e585fcac15a79a823ee1301d
Hemant <jainhemant126@gmail.com>
2022-05-13 15:21:17 +0530

    Session marks implemented

--- backend/controller/markscontroller.js
+++ backend/controller/markscontroller.js
@@ -32,6 +32,57 @@ const specificstudent = async(req,res) => {
     }
     res.send(student)
     // console.log(student.sessionmarks)
+} 
+const specificsessionmarks = async(req,res) => {
+    const roll = req.params.roll;
+    const session = req.params.session;
+    const student = await Student.findOne({reg_no : parseInt(roll)})
+    // {session : parseInt(session)}
+    let sessionmarks = student.sessionmarks
+
+    if(!student){
+        return res.status(400).json({
+            'message' : 'Missing'
+        })
+    }
+    // console.log(sessionmarks)
+    var subjectmarks = sessionmarks.find(item => item.session == session)
+    res.send(subjectmarks.subjectmarks)
+}
+const totalsessions = async(req,res) => {
+  const roll = req.params.roll;
+  const student = await Student.findOne({reg_no : parseInt(roll)})
+  // {session : parseInt(session)}
+  let sessionmarks = student.sessionmarks
+
+  if(!student){
+      return res.status(400).json({
+          'message' : 'Missing'
+      })
+  }
+  // console.log(sessionmarks)
+  var sessions = sessionmarks.map(item => item.session)
+  res.send(sessions)
+}
+const specificsubjects = async(req,res) => {
+  const roll = req.params.roll;
+  const student = await Student.findOne({reg_no : parseInt(roll)})
+  // {session : parseInt(session)}
+  const session = req.params.session;
+  let sessionmarks = student.sessionmarks
+
+  if(!student){
+      return res.status(400).json({
+          'message' : 'Missing'
+      })
+  }
+  // console.log(sessionmarks)
+  var subjectmark = sessionmarks.find(item => item.session == session)
+  var subjectmarks=subjectmark.subjectmarks
+  // console.log(subjectmarks);
+  var subjects = subjectmarks.map(value => value.subject)
+  // console.log(subjects);
+  res.send(subjects)
 }
 
-module.exports = { getstudentmarks, poststudentmarks,specificstudent}
+module.exports = { getstudentmarks, poststudentmarks,specificstudent,specificsessionmarks,specificsubjects,totalsessions}
--- backend/routes/marks.js
+++ backend/routes/marks.js
@@ -5,5 +5,8 @@ const student = require('../controller/markscontroller')
 router.route('/').get(student.getstudentmarks)
 router.route('/').post(student.poststudentmarks)
 router.route('/:roll').get(student.specificstudent)
+router.route('/:roll/sessions').get(student.totalsessions)
+router.route('/:roll/:session').get(student.specificsessionmarks)
+router.route('/:roll/:session/subjects').get(student.specificsubjects)
 
 module.exports = router;
